ef377057e787a3530f3d2e4c6dcf02688e9f3622
VEX 1868 Team X <31676837+VEX1868TeamX@users.noreply.github.com>
2019-11-26 19:12:19 -0800

    bellarmine changes and updates
    changes to both different interfaces of code before and after bellarmine

--- TowerTakeoverXComp/src/main.cpp
+++ TowerTakeoverXComp/src/main.cpp
@@ -16,7 +16,7 @@
 // ArmMotorRight        motor         18              
 // ArmMotorLeft         motor         11              
 // FlapMotor2           motor         1               
-// FlapMotor1           motor         20              
+// FlapMotor1           motor         17              
 // CenterMotor          motor         21              
 // Controller1          controller                    
 // Controller2          controller                    
@@ -25,91 +25,101 @@
 #include "vex.h"
 competition Competition;
 using namespace vex;
-void driveChassis (double distance, double sideways){
+void driveChassis(double distance, double sideways) {
   LeftMotor.spinFor(distance, turns);
   RightMotor.spinFor(distance, turns);
-  CenterMotor.spinFor(sideways, turns); 
+  CenterMotor.spinFor(sideways, turns);
 }
-void turnChassis (double distance, double sideways){
-  LeftMotor.spinFor(distance,turns);
-  RightMotor.spinFor(distance,turns);
-  CenterMotor.spinFor(sideways,turns); //only for moving diagonally 
+void turnChassis(double distance, double sideways) {
+  LeftMotor.spinFor(distance, turns);
+  RightMotor.spinFor(distance, turns);
+  CenterMotor.spinFor(sideways, turns); // only for moving diagonally
 }
-void moveRamp (double angle){
-  RCMotor.spinToPosition(angle, degrees);
+void moveRamp(double angle) { 
+  RCMotor.spinToPosition(angle, degrees); 
 }
-void moveArms (double angle){
+void moveArms(double angle) {
   ArmMotorLeft.spinToPosition(angle, degrees);
   ArmMotorRight.spinToPosition(angle, degrees);
 }
 
-void pre_auton (){
+void simpleAuton(void) { //scores the preload and releases the intake rollers
+  driveChassis(-50, 0); //moves forward 50 rotations
+  driveChassis(50, 0); //moves back 50 rotations
+  driveChassis(0, 0); //stops the chassis
+  moveArms(45); //moves the arms up to the position 45 degrees to releace the intake rollers
+  moveArms(0); //moves the arms back down to the original position, ready for the start of the match
+}
+void redSquareLeftauton(void){
+
+}
+
+void pre_auton() {
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
   Controller1.Screen.print("MAIN");
   Controller2.Screen.print("PARTNER");
-  Brain.Screen.print (Brain.Battery.capacity());
-  //Brain.Screen.printAt (20, 40, "Red Alliance, Right Square");
+  Brain.Screen.print(Brain.Battery.capacity());
+  // Brain.Screen.printAt (20, 40, "Red Alliance, Right Square");
   RCMotor.resetRotation();
   ArmMotorLeft.resetRotation();
   ArmMotorRight.resetRotation();
 }
-void autonomous(){
+void autonomous() {
 
 }
-void drivercontrol (){
-//double leftSpeed, rightSpeed;
-double x, y, z;
-  
-  while(1) {
-      /* 
-      leftSpeed = Controller1.Axis3.value();
-      rightSpeed = Controller1.Axis2.value ();
-      LeftMotor.spin(vex::directionType::fwd, leftSpeed, vex::velocityUnits::pct);
-      RightMotor.spin(vex::directionType::fwd, rightSpeed, vex::velocityUnits::pct);
-      */
-      //chassis drive
-      x = Controller1.Axis4.value();
-      y = Controller1.Axis3.value();
-      z = Controller1.Axis1.value();
-      LeftMotor.spin(vex::directionType::fwd, y+x, vex::velocityUnits::pct);
-      RightMotor.spin(vex::directionType::fwd, y-x, vex::velocityUnits::pct);
-      CenterMotor.spin(vex::directionType::fwd, -z*0.5 , vex::velocityUnits::pct);
-      //arm control -- moves the arm up and down
-      ArmMotorLeft.spin(vex::directionType::fwd,Controller2.Axis3.value(),vex::velocityUnits::pct);
-      ArmMotorRight.spin(vex::directionType::rev,Controller2.Axis2.value(),vex::velocityUnits::pct);
-      
-      //flaps -- goes around in circles to pick up the cube
-      if(Controller2.ButtonUp.pressing()) {
-          FlapMotor1.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
-          FlapMotor2.spin(vex::directionType::fwd, 100, vex::velocityUnits::pct);
-      } 
-      else if (Controller2.ButtonDown.pressing()){
-          FlapMotor1.spin(vex::directionType::rev, 100, vex::velocityUnits::pct);
-          FlapMotor2.spin(vex::directionType::rev, 100, vex::velocityUnits::pct);
-      }
-      else {
-          FlapMotor2.stop();
-          FlapMotor1.stop();
-      }
-      
-      
-      //Ramp control
-      RCMotor.spin(vex::directionType::fwd,Controller2.Axis1.value(),vex::velocityUnits::pct);
-      
-      vex::task::sleep(20); //leave this at end of the while loop
-  }
+void drivercontrol() {
+  double leftSpeed, rightSpeed, centerSpeed;
+  // double x, y, z;
 
+  while (1) {
+    // chassis drive
+
+    leftSpeed = Controller1.Axis3.value();
+    rightSpeed = Controller1.Axis2.value();
+    centerSpeed = Controller1.Axis1.value();
+    LeftMotor.spin(forward, leftSpeed, pct);
+    RightMotor.spin(forward, rightSpeed, pct);
+    CenterMotor.spin(forward, centerSpeed, pct);
+
+    /*
+    x = Controller1.Axis1.value();
+    y = Controller1.Axis2.value();
+    z = Controller1.Axis4.value();
+    LeftMotor.spin( forward, y+x,  pct);
+    RightMotor.spin( forward, y-x,  pct);
+    CenterMotor.spin( forward, -z*0.5 ,  pct);
+    */
+
+    ArmMotorLeft.spin(forward, Controller2.Axis3.value(),  pct);
+    ArmMotorRight.spin(forward, Controller2.Axis2.value(),  pct);
+
+    // flaps -- goes around in circles to pick up the cube
+    if (Controller2.ButtonUp.pressing()) {
+      FlapMotor1.spin(forward, 100,  pct);
+      FlapMotor2.spin(forward, 100,  pct);
+    } else if (Controller2.ButtonDown.pressing()) {
+      FlapMotor1.spin(reverse, 100,  pct);
+      FlapMotor2.spin(reverse, 100,  pct);
+    } else {
+      FlapMotor2.stop();
+      FlapMotor1.stop();
+    }
+
+    // Ramp control
+    RCMotor.spin( forward, Controller2.Axis1.value(), pct);
+
+    vex::task::sleep(20); // leave this at end of the while loop
+  }
 }
 
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-  //Run the pre-autonomous function. 
+  // Run the pre-autonomous function.
   pre_auton();
 
-  //Set up callbacks for autonomous and driver control periods.
+  // Set up callbacks for autonomous and driver control periods.
   Competition.autonomous(autonomous);
   Competition.drivercontrol(drivercontrol);
-
 }
