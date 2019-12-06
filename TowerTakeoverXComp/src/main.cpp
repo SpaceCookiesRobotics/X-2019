4ac171ad092ef47858dcb0e0c28912e03c67e06c
VEX 1868 Team X <31676837+VEX1868TeamX@users.noreply.github.com>
2019-12-05 18:55:18 -0800

    Comments added to the code to make it easier to understand
    Added codes to the current version to make it easier to understand what each line of code does in the event of the absence of a programmer

--- TowerTakeoverXComp/src/main.cpp
+++ TowerTakeoverXComp/src/main.cpp
@@ -10,110 +10,133 @@
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // Robot Configuration:
 // [Name]               [Type]        [Port(s)]
-// RightMotor           motor         10              
-// LeftMotor            motor         3               
-// RCMotor              motor         2               
-// ArmMotorRight        motor         18              
-// ArmMotorLeft         motor         11              
-// FlapMotor2           motor         1               
-// FlapMotor1           motor         17              
-// CenterMotor          motor         21              
-// Controller1          controller                    
-// Controller2          controller                    
+// RightMotor           motor         10
+// LeftMotor            motor         3
+// RCMotor              motor         2
+// ArmMotorRight        motor         18
+// ArmMotorLeft         motor         11
+// FlapMotor2           motor         1
+// FlapMotor1           motor         17
+// CenterMotor          motor         21
+// Controller1          controller
+// Controller2          controller
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
 competition Competition;
 using namespace vex;
-void driveChassis(double distance, double sideways) {
+void driveChassis(double distance, double sideways) { //moves the chassis motors a certain number of turns so the chassis can move linearlly
   LeftMotor.spinFor(distance, turns);
   RightMotor.spinFor(distance, turns);
-  CenterMotor.spinFor(sideways, turns);
+  CenterMotor.spinFor(sideways, turns);//only needed for moving sideways
 }
-void turnChassis(double distance, double sideways) {
+void turnChassis(double distance, double sideways) { //moves the chassis motors a certain number of turns so the chassis can turn
   LeftMotor.spinFor(distance, turns);
-  RightMotor.spinFor(distance, turns);
+  RightMotor.spinFor(-distance, turns);
   CenterMotor.spinFor(sideways, turns); // only for moving diagonally
 }
-void moveRamp(double angle) { 
-  RCMotor.spinToPosition(angle, degrees); 
-}
-void moveArms(double angle) {
+void moveRamp(double angle) { //moves the ramp motor to a certain angle
+  RCMotor.spinToPosition(angle, degrees);
+ }
+void moveArms(double angle) { //moves the arms to a certain angle
   ArmMotorLeft.spinToPosition(angle, degrees);
   ArmMotorRight.spinToPosition(angle, degrees);
 }
 
-void simpleAuton(void) { //scores the preload and releases the intake rollers
-  driveChassis(-50, 0); //moves forward 50 rotations
-  driveChassis(50, 0); //moves back 50 rotations
-  driveChassis(0, 0); //stops the chassis
-  moveArms(45); //moves the arms up to the position 45 degrees to releace the intake rollers
-  moveArms(0); //moves the arms back down to the original position, ready for the start of the match
+void simpleAuton(void) { // scores the preload and releases the intake rollers
+  driveChassis(-50, 0);  // moves forward 50 rotations
+  driveChassis(50, 0);   // moves back 50 rotations
+  driveChassis(0, 0);    // stops the chassis
+  moveArms(45); // moves the arms up to the position 45 degrees to releace the intake rollers
+  moveArms(0);  // moves the arms back down to the original position, ready for the start of the match
 }
-void redSquareLeftauton(void){
+void redSquareLeftauton(void) {
 
 }
 
 void pre_auton() {
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
+//labeling the controllers during matches
   Controller1.Screen.print("MAIN");
   Controller2.Screen.print("PARTNER");
+  //displays the percentage of batter level left for the robot when the code is turned on
   Brain.Screen.print(Brain.Battery.capacity());
+  //displays which auton code is running <-- currently not necessarily needed 
   // Brain.Screen.printAt (20, 40, "Red Alliance, Right Square");
+  
+  //resets the encoder values for the ramp control & arms 
+  //useful for autonomous codes
   RCMotor.resetRotation();
   ArmMotorLeft.resetRotation();
   ArmMotorRight.resetRotation();
 }
 void autonomous() {
-
+  simpleAuton();//scores the preload and flips out the intake rollers
 }
+
 void drivercontrol() {
+  //defines the different variables for chassis code
   double leftSpeed, rightSpeed, centerSpeed;
   // double x, y, z;
 
   while (1) {
     // chassis drive
-
-    leftSpeed = Controller1.Axis3.value();
-    rightSpeed = Controller1.Axis2.value();
-    centerSpeed = Controller1.Axis1.value();
-    LeftMotor.spin(forward, leftSpeed, pct);
-    RightMotor.spin(forward, rightSpeed, pct);
-    CenterMotor.spin(forward, centerSpeed, pct);
+    leftSpeed = Controller1.Axis3.value(); //defines the axis for the left side of the chassis
+    rightSpeed = Controller1.Axis2.value(); //defines the axis for the right side of the chasssis
+    centerSpeed = Controller1.Axis1.value(); //defines the axis for the 5th wheel
+    
+    //tank drive with the 5th wheel
+    LeftMotor.spin(forward, leftSpeed, pct); //left side
+    RightMotor.spin(forward, rightSpeed, pct); //right side
+    CenterMotor.spin(forward, centerSpeed, pct); //5th wheel
 
     /*
-    x = Controller1.Axis1.value();
-    y = Controller1.Axis2.value();
-    z = Controller1.Axis4.value();
-    LeftMotor.spin( forward, y+x,  pct);
-    RightMotor.spin( forward, y-x,  pct);
-    CenterMotor.spin( forward, -z*0.5 ,  pct);
+    x = Controller1.Axis1.value(); //defines the axis for left and right
+    y = Controller1.Axis2.value(); //defines the axis for forwards and backwards
+    z = Controller1.Axis4.value(); //defines the axis for sideways (aka the 5th wheel)
+    //Holonomic drive
+    LeftMotor.spin( forward, y+x,  pct); //left side
+    RightMotor.spin( forward, y-x,  pct); //right side
+    CenterMotor.spin( forward, -z*0.5 ,  pct); //5th wheel
     */
     // arms control
-    ArmMotorLeft.spin(forward, Controller2.Axis3.value(),  pct);
-    ArmMotorRight.spin(forward, Controller2.Axis3.value(),  pct);
-
-  if (Controller2.ButtonX.pressed(void)){
-    ArmMotorLeft.spinToPosition(45, degrees);
-    ArmMotorRight.spinToPosition(45, degrees);
-  }
-
+    if (Controller2.ButtonX.pressing()) {
+      // move the motors up so they are parallel to the ground
+      ArmMotorLeft.spinToPosition(45, degrees);
+      ArmMotorRight.spinToPosition(45, degrees);
+      //the motors hold the arms at that position
+      ArmMotorLeft.setStopping(hold);
+      ArmMotorRight.setStopping(hold);
+    } else if (Controller2.ButtonR1.pressing()) {
+      // move the motors up part of the way
+      ArmMotorLeft.spinToPosition(22.5, degrees); 
+      ArmMotorRight.spinToPosition(22.5, degrees);
+      //the motors hold the arms at that position
+      ArmMotorLeft.setStopping(hold);
+      ArmMotorRight.setStopping(hold);
+    } else {
+      //the regular control for the arms
+      // both are controlled by moving the left pot(joystick) on the 2nd controller up and down
+      ArmMotorLeft.spin(forward, Controller2.Axis3.value(), pct);
+      ArmMotorRight.spin(forward, Controller2.Axis3.value(), pct);
+    }
 
     // flaps -- goes around in circles to pick up the cube
     if (Controller2.ButtonUp.pressing()) {
-      FlapMotor1.spin(forward, 100,  pct);
-      FlapMotor2.spin(forward, 100,  pct);
+      //moves the intake rollers so they intake cubes
+      FlapMotor1.spin(forward, 100, pct);
+      FlapMotor2.spin(forward, 100, pct);
     } else if (Controller2.ButtonDown.pressing()) {
-      FlapMotor1.spin(reverse, 100,  pct);
-      FlapMotor2.spin(reverse, 100,  pct);
+      //moves the intake rollers so they outtake cubes
+      FlapMotor1.spin(reverse, 100, pct);
+      FlapMotor2.spin(reverse, 100, pct);
     } else {
+      //keeps the motors from moving when the buttons are not pressed
       FlapMotor2.stop();
       FlapMotor1.stop();
     }
 
     // Ramp control
-    RCMotor.spin( forward, Controller2.Axis4.value(), pct);
+    RCMotor.spin(forward, Controller2.Axis4.value(), pct); //moves the cube ramp up and down
 
     vex::task::sleep(20); // leave this at end of the while loop
   }
